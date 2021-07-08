f3e54447e2fef99a38eb7474956ed2ed2d580e33
Catherine Pacheco <analistaoperaciones3principal@cofincafecloud.local>
2021-07-08 18:56:02 -0500

    se crea proyecto java de cine

--- TIENDA/src/cine/src/ClientePremiun.java
+++ TIENDA/src/cine/src/ClientePremiun.java
@@ -1,4 +1,10 @@
-public class ClientePremiun extends Persona {
+public class ClientePremiun extends Persona implements Tiquete {
     
     private int numeroTarjeta;
+
+    @Override
+    public float calcularCosto(float costoBase) {
+        float costoReal = (float)(costoBase - (costoBase * 0.5));
+        return costoReal;
+    }
 }
